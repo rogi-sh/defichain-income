227c244b961fe75a808714300a608f79ce092f79
Igor (admin) <is@topiet.de>
2021-02-12 12:59:47 +0100

    deploy fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  dex: 'https://api.defichain.io/v1/listyieldfarming?network=mainnet',
+  poolDetails: 'https://api.defichain.io/v1/getpoolpair?id='
 };
