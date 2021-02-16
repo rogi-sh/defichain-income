5ec9389d2d38753986f818f91d10ff756465ed2a
Igor (admin) <is@topiet.de>
2021-02-16 12:35:49 +0100

    staking calucaltion for target return

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -61,6 +61,13 @@ export class AppComponent implements OnInit {
   dfiInStakingKey = 'dfiInStaking';
   dfiInStaking = 11050;
   stakingApy = 37;
+
+  stakingNeededForAimReturnMin = 0;
+  aimReturnMin = 0;
+
+  stakingNeededForAimReturnHour = 0;
+  aimReturnHour = 0;
+
   stakingNeededForAimReturnDay = 0;
   aimReturnDay = 0;
 
@@ -292,6 +299,16 @@ export class AppComponent implements OnInit {
   onChangeAimReturnDay(newValue): void {
     const apyPerDay =  Math.pow(1 + this.stakingApy / 100, 1 / 365) - 1;
     this.stakingNeededForAimReturnDay = newValue / apyPerDay * (100 + apyPerDay) * (1 / (1 + apyPerDay / 100)) / 100;
+  }
+
+  onChangeAimReturnHour(newValue): void {
+    const apyPerHour =  Math.pow(1 + this.stakingApy / 100, 1 / 8760) - 1;
+    this.stakingNeededForAimReturnHour = newValue / apyPerHour * (100 + apyPerHour) * (1 / (1 + apyPerHour / 100)) / 100;
+  }
+
+  onChangeAimReturnMin(newValue): void {
+    const apyPerMin =  Math.pow(1 + this.stakingApy / 100, 1 / 525600) - 1;
+    this.stakingNeededForAimReturnMin = newValue / apyPerMin * (100 + apyPerMin) * (1 / (1 + apyPerMin / 100)) / 100;
   }
 
   onChangeAimReturnMonth(newValue): void {
