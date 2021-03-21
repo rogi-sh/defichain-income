f97228c23895c8b79a32ba91122d29b0c4d0f243
Igor Shelkovenkov <ishelkovenkov.oev@gmail.com>
2021-03-21 17:59:34 +0100

    btc and eth in coinpaprika widget closes igor/defichain-income#18

--- src/interface/Changelog.ts
+++ src/interface/Changelog.ts
@@ -13,6 +13,13 @@ export class Changelog {
 
     this.log = new Log();
 
+    this.log.version = '2.21.0 - 21.3.2021';
+    this.log.comment = 'Add btc and eth as currency for coinpaprika widget';
+
+    this.logs.push(this.log);
+
+    this.log = new Log();
+
     this.log.version = '2.20.0 - 21.3.2021';
     this.log.comment = 'Add reference to history graphql interface';
 
