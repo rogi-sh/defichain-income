f2b86515a23025a3f664f564de633714f4d95c7d
Igor Shelkovenkov <ishelkovenkov.oev@gmail.com>
2021-04-26 12:17:37 +0200

    better wording

--- src/interface/Changelog.ts
+++ src/interface/Changelog.ts
@@ -13,6 +13,13 @@ export class Changelog {
 
     this.log = new Log();
 
+    this.log.version = '4.6.3 - 26.4.2021';
+    this.log.comment = 'better wording and headlines';
+
+    this.logs.push(this.log);
+
+    this.log = new Log();
+
     this.log.version = '4.6.2 - 26.4.2021';
     this.log.comment = 'fix no custom rewards';
 
