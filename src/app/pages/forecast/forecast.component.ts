33ddcc7af3c8910ef98ba069fd6a84bbea40cf40
Antoni Kucharczyk <hello@antoni.work>
2021-05-29 18:32:57 +0200

    Add world icon change background style Code style

--- src/app/pages/forecast/forecast.component.ts
+++ src/app/pages/forecast/forecast.component.ts
@@ -1,15 +1,25 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
-import {MasternodeOutcome, Outcome, OutcomeStaking, PoolAllOut} from '@interfaces/Dex';
-import {ChartComponent} from 'ng-apexcharts';
-import {ChartOptions5} from '@interfaces/Data';
-
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
+import {
+  MasternodeOutcome,
+  Outcome,
+  OutcomeStaking,
+  PoolAllOut,
+} from '@interfaces/Dex';
+import { ChartComponent } from 'ng-apexcharts';
+import { ChartOptions5 } from '@interfaces/Data';
 
 @Component({
   selector: 'app-forecast',
-  templateUrl: './forecast.component.html'
+  templateUrl: './forecast.component.html',
 })
 export class ForecastComponent implements OnInit, OnChanges {
-
   @ViewChild('chartForecast') chart: ChartComponent;
   public chartOptions: Partial<ChartOptions5>;
 
@@ -32,17 +42,17 @@ export class ForecastComponent implements OnInit, OnChanges {
   @Input()
   bs!: number;
 
-  poolOuts =  new Array<PoolAllOut>();
+  poolOuts = new Array<PoolAllOut>();
 
-  poolLmOuts =  new Array<PoolAllOut>();
+  poolLmOuts = new Array<PoolAllOut>();
 
-  poolStakingOuts =  new Array<PoolAllOut>();
+  poolStakingOuts = new Array<PoolAllOut>();
 
-  poolMnOuts =  new Array<PoolAllOut>();
+  poolMnOuts = new Array<PoolAllOut>();
 
   blocksPerCycle = 32690;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.computeMasternodesReduce();
@@ -54,33 +64,28 @@ export class ForecastComponent implements OnInit, OnChanges {
   }
 
   computeMasternodesReduce(): void {
-
     if (this.poolOut === undefined || this.poolOut.dfiPerDay === undefined) {
       return;
     }
 
     // clean
-    this.poolOuts =  new Array<PoolAllOut>();
-    this.poolLmOuts =  new Array<PoolAllOut>();
-    this.poolStakingOuts =  new Array<PoolAllOut>();
-    this.poolMnOuts =  new Array<PoolAllOut>();
+    this.poolOuts = new Array<PoolAllOut>();
+    this.poolLmOuts = new Array<PoolAllOut>();
+    this.poolStakingOuts = new Array<PoolAllOut>();
+    this.poolMnOuts = new Array<PoolAllOut>();
 
     for (let i = 0; i <= this.timeHorizonCycles; i++) {
-
       let poolAllOut = new PoolAllOut();
       let poolStakingOut = new PoolAllOut();
       let poolLmOut = new PoolAllOut();
       let poolMnOut = new PoolAllOut();
-
 
       if (i === 0) {
         this.transformPool(poolAllOut, this.poolOut);
         this.transformPool(poolStakingOut, this.stakingOut);
         this.transformPool(poolLmOut, this.lmOut);
         this.transformPool(poolMnOut, this.poolMasternodeOut);
-
       } else {
-
         this.transformPoolReduced(poolAllOut, this.poolOuts, i);
         this.transformPoolReduced(poolStakingOut, this.poolStakingOuts, i);
         this.transformPoolReduced(poolLmOut, this.poolLmOuts, i);
@@ -92,46 +97,44 @@ export class ForecastComponent implements OnInit, OnChanges {
       this.poolMnOuts.push(poolMnOut);
       this.poolStakingOuts.push(poolStakingOut);
     }
-
   }
 
   buildChart(): void {
-
     this.chartOptions = {
       series: [
         {
           name: 'Staking',
-          data: this.getStakingData()
+          data: this.getStakingData(),
         },
         {
           name: 'LM',
-          data: this.getLMData()
+          data: this.getLMData(),
         },
         {
           name: 'Masternode',
-          data: this.getMNData()
+          data: this.getMNData(),
         },
         {
           name: 'All',
-          data: this.getAllData()
-        }
+          data: this.getAllData(),
+        },
       ],
       chart: {
         type: 'area',
-        height: 600
+        height: 600,
+        background: 'transparent',
       },
       dataLabels: {
-        enabled: false
+        enabled: false,
       },
       stroke: {
-        curve: 'smooth'
+        curve: 'smooth',
       },
       xaxis: {
         type: 'category',
-        categories: this.getCat()
-      }
+        categories: this.getCat(),
+      },
     };
-
   }
 
   onChangeLmCalculatIncome(value: number) {
@@ -142,32 +145,41 @@ export class ForecastComponent implements OnInit, OnChanges {
 
   getStakingData(): Array<number> {
     let result = new Array<number>();
-    this.poolStakingOuts.forEach(p => result.push(Math.round(p.dfiPerMonth * 100) / 100));
+    this.poolStakingOuts.forEach((p) =>
+      result.push(Math.round(p.dfiPerMonth * 100) / 100)
+    );
     return result;
   }
 
   getLMData(): Array<number> {
     let result = new Array<number>();
-    this.poolLmOuts.forEach(p => result.push(Math.round(p.dfiPerMonth * 100) / 100));
+    this.poolLmOuts.forEach((p) =>
+      result.push(Math.round(p.dfiPerMonth * 100) / 100)
+    );
     return result;
   }
 
   getMNData(): Array<number> {
     let result = new Array<number>();
-    this.poolMnOuts.forEach(p => result.push(Math.round(p.dfiPerMonth * 100) / 100));
+    this.poolMnOuts.forEach((p) =>
+      result.push(Math.round(p.dfiPerMonth * 100) / 100)
+    );
     return result;
   }
 
   getAllData(): Array<number> {
     let result = new Array<number>();
-    this.poolOuts.forEach(p => result.push(Math.round(p.dfiPerMonth * 100) / 100));
+    this.poolOuts.forEach((p) =>
+      result.push(Math.round(p.dfiPerMonth * 100) / 100)
+    );
     return result;
   }
 
   getCat(): Array<string> {
     let result = new Array<string>();
-    const options = {year: 'numeric', month: 'long', day: 'numeric' };
-    let date = new Date(2021, 5, 3) > new Date() ? new Date(2021, 5, 3) : new Date();
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    let date =
+      new Date(2021, 5, 3) > new Date() ? new Date(2021, 5, 3) : new Date();
 
     for (let i = 0; i <= this.timeHorizonCycles; i++) {
       result.push(i + ' : ' + date.toLocaleDateString('de-DE', options));
@@ -184,7 +196,11 @@ export class ForecastComponent implements OnInit, OnChanges {
     poolAllOut.dfiPerYear = poolInput.dfiPerYear;
   }
 
-  transformPoolReduced(poolAllOut: PoolAllOut, inputPool: Array<PoolAllOut>, i: number): void {
+  transformPoolReduced(
+    poolAllOut: PoolAllOut,
+    inputPool: Array<PoolAllOut>,
+    i: number
+  ): void {
     poolAllOut.dfiPerDay = inputPool[i - 1].dfiPerDay * this.reducePercent;
     poolAllOut.dfiPerMin = inputPool[i - 1].dfiPerMin * this.reducePercent;
     poolAllOut.dfiPerHour = inputPool[i - 1].dfiPerHour * this.reducePercent;
@@ -195,5 +211,4 @@ export class ForecastComponent implements OnInit, OnChanges {
   getTheme(): string {
     return localStorage.getItem('theme');
   }
-
 }
