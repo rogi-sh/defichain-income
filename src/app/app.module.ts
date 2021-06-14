15bc49853b2ecb7a932a52097020c3046269bc9b
Igor Shelkovenkov <ishelkovenkov.oev@gmail.com>
2021-06-14 09:24:56 +0200

    cleaning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,10 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { FormsModule } from '@angular/forms';
 import { NgApexchartsModule } from 'ng-apexcharts';
 import { CountdownModule } from 'ngx-countdown';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { MatomoModule } from 'ngx-matomo-v9';
-
 import { GraphQLModule } from './graphql.module';
-
 import { ApyCalculatorComponent } from '@components/apy-calculator/apy-calculator.component';
 import { ChangelogComponent } from '@components/changelog/changelog.component';
 import { DexStatisticsComponent } from '@components/dex-statistics/dex-statistics.component';
@@ -25,13 +22,12 @@ import { LmTargetCalculatorComponent } from '@components/lm-target-calculator/lm
 import { StakingTargetCalculatorComponent } from '@components/staking-target-calculator/staking-target-calculator.component';
 import { ValueComponent } from '@components/value/value.component';
 import { IconComponent } from '@components/icon/icon.component';
-
 import { CalculatorComponent } from '@pages/calculator/calculator.component';
 import { InfoComponent } from '@pages/info/info.component';
 import { PriceComponent } from '@components/price/price.component';
 import {DataService} from '@services/data.service';
 import {NgxSpinnerModule} from 'ngx-spinner';
-import { ForecastComponent } from './pages/forecast/forecast.component';
+import { ForecastComponent } from '@pages/forecast/forecast.component';
 
 @NgModule({
   declarations: [
@@ -61,7 +57,6 @@ import { ForecastComponent } from './pages/forecast/forecast.component';
     FormsModule,
     NgApexchartsModule,
     CountdownModule,
-    // ngx-translate and the loader module
     HttpClientModule,
     NgxSpinnerModule,
     TranslateModule.forRoot({
