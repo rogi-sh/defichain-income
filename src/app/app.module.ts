8c72991f831cbbca175825e7f5ee99972f5695f3
Antoni Kucharczyk <hello@antoni.work>
2021-07-14 18:45:06 +0200

    #56
    Add pwa
    Fix styles
    
    /spend 25m

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,8 @@ import {DataService} from '@services/data.service';
 import {NgxSpinnerModule} from 'ngx-spinner';
 import { ForecastComponent } from '@pages/forecast/forecast.component';
 import { ToastrModule } from 'ngx-toastr';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -74,6 +76,12 @@ import { ToastrModule } from 'ngx-toastr';
       },
     }),
     GraphQLModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
