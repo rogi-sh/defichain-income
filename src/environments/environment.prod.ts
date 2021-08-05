cec30d3d2e52017f7a9b63e024d233f144e758a8
Igor Shelkovenkov <ishelkovenkov.oev@gmail.com>
2021-08-05 12:22:09 +0200

    Endpoint only inclusding_start

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   listyieldfarming: 'https://api.defichain.io/v1/listyieldfarming?network=mainnet',
   poolDetails: 'https://api.defichain.io/v1/getpoolpair?id=',
   listpoolpairs: 'https://api.defichain.io/v1/listpoolpairs',
-  accountDetails: 'https://api.defichain.io/v1/getaccount?start=0&including_start=true&owner=',
+  accountDetails: 'https://api.defichain.io/v1/getaccount?including_start=true&owner=',
   balance: 'https://mainnet-api.defichain.io/api/DFI/mainnet/address/SET-ADDRESS/balance',
   health: 'https://api.defichain.io/ok',
   stats: 'https://api.defichain.io/v1/stats',
