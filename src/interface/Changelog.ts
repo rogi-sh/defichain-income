ff32fff7e2bb27014fd5329f4849d74c7c16e828
Igor Shelkovenkov <ishelkovenkov.oev@gmail.com>
2021-08-08 17:00:39 +0200

    generell/defichain-income/defichain-income#55 Fix show added address success message

--- src/interface/Changelog.ts
+++ src/interface/Changelog.ts
@@ -13,6 +13,13 @@ export class Changelog {
 
     this.log = new Log();
 
+    this.log.version = '7.9.3 - 8.8.2021';
+    this.log.comment = 'Fix show added address success message';
+
+    this.logs.push(this.log);
+
+    this.log = new Log();
+
     this.log.version = '7.9.2 - 8.8.2021';
     this.log.comment = 'Fix wrong adresses balance requested';
 
