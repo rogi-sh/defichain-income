9fe1f719ef1a00919fcd6b5e60fd4771ea9ae76b
Igor Shelkovenkov <is@topiet.de>
2021-10-06 13:22:40 +0200

    LP Token Value added to dex statistics table closes generell/defichain-income/defichain-income#69

--- src/interface/Changelog.ts
+++ src/interface/Changelog.ts
@@ -12,6 +12,11 @@ export class Changelog {
     this.logs = new Array<Log>();
 
     this.log = new Log();
+    this.log.version = '7.16.0 - 6.10.2021';
+    this.log.comment = 'LP Token Value added to dex statistics table';
+    this.logs.push(this.log);
+
+    this.log = new Log();
     this.log.version = '7.15.7 - 30.9.2021';
     this.log.comment = 'Fix Release text from github if structure not like normal';
     this.logs.push(this.log);
