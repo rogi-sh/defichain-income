7eb94b03a7a3baab27ff2885fedd15db1bead3a5
igorshelkovenkov <is@topiet.de>
2021-12-04 20:48:59 +0100

    add new pools to calculator

--- src/app/components/lm-target-calculator/lm-target-calculator.component.ts
+++ src/app/components/lm-target-calculator/lm-target-calculator.component.ts
@@ -36,6 +36,48 @@ export class LmTargetCalculatorComponent implements OnInit {
   poolTsla: Pool;
 
   @Input()
+  poolQqq!: Pool;
+
+  @Input()
+  poolSpy!: Pool;
+
+  @Input()
+  poolPltr!: Pool;
+
+  @Input()
+  poolSlv!: Pool;
+
+  @Input()
+  poolAapl!: Pool;
+
+  @Input()
+  poolGld!: Pool;
+
+  @Input()
+  poolGme!: Pool;
+
+  @Input()
+  poolGoogl!: Pool;
+
+  @Input()
+  poolArkk!: Pool;
+
+  @Input()
+  poolBaba!: Pool;
+
+  @Input()
+  poolVnq!: Pool;
+
+  @Input()
+  poolUrth!: Pool;
+
+  @Input()
+  poolTlt!: Pool;
+
+  @Input()
+  poolPdbc!: Pool;
+
+  @Input()
   dfiProBlockBtc;
 
   @Input()
@@ -61,6 +103,48 @@ export class LmTargetCalculatorComponent implements OnInit {
 
   @Input()
   dfiProBlockTsla;
+
+  @Input()
+  dfiProBlockQqq;
+
+  @Input()
+  dfiProBlockSpy;
+
+  @Input()
+  dfiProBlockPltr;
+
+  @Input()
+  dfiProBlockSlv;
+
+  @Input()
+  dfiProBlockAapl;
+
+  @Input()
+  dfiProBlockGld;
+
+  @Input()
+  dfiProBlockGme;
+
+  @Input()
+  dfiProBlockGoogl;
+
+  @Input()
+  dfiProBlockArkk;
+
+  @Input()
+  dfiProBlockBaba;
+
+  @Input()
+  dfiProBlockVnq;
+
+  @Input()
+  dfiProBlockUrth;
+
+  @Input()
+  dfiProBlockTlt;
+
+  @Input()
+  dfiProBlockPdbc;
 
   @Input()
   blocktimeInS;
@@ -163,6 +247,48 @@ export class LmTargetCalculatorComponent implements OnInit {
     } else if (this.poolLmCalculationTargetReturn === 'TSLA'){
       pool = this.poolTsla;
       dfiPerBlock = this.dfiProBlockTsla;
+    } else if (this.poolLmCalculationTargetReturn === 'SPY'){
+      pool = this.poolSpy;
+      dfiPerBlock = this.dfiProBlockSpy;
+    } else if (this.poolLmCalculationTargetReturn === 'QQQ'){
+      pool = this.poolQqq;
+      dfiPerBlock = this.dfiProBlockQqq;
+    } else if (this.poolLmCalculationTargetReturn === 'PLTR'){
+      pool = this.poolPltr;
+      dfiPerBlock = this.dfiProBlockPltr;
+    } else if (this.poolLmCalculationTargetReturn === 'SLV'){
+      pool = this.poolSlv;
+      dfiPerBlock = this.dfiProBlockSlv;
+    } else if (this.poolLmCalculationTargetReturn === 'AAPL'){
+      pool = this.poolAapl;
+      dfiPerBlock = this.dfiProBlockAapl;
+    } else if (this.poolLmCalculationTargetReturn === 'GLD'){
+      pool = this.poolGld;
+      dfiPerBlock = this.dfiProBlockGld;
+    } else if (this.poolLmCalculationTargetReturn === 'GME'){
+      pool = this.poolGme;
+      dfiPerBlock = this.dfiProBlockGme;
+    } else if (this.poolLmCalculationTargetReturn === 'GOOGL'){
+      pool = this.poolGoogl;
+      dfiPerBlock = this.dfiProBlockGoogl;
+    } else if (this.poolLmCalculationTargetReturn === 'ARKK'){
+      pool = this.poolArkk;
+      dfiPerBlock = this.dfiProBlockArkk;
+    } else if (this.poolLmCalculationTargetReturn === 'BABA'){
+      pool = this.poolBaba;
+      dfiPerBlock = this.dfiProBlockBaba;
+    } else if (this.poolLmCalculationTargetReturn === 'VNQ'){
+      pool = this.poolVnq;
+      dfiPerBlock = this.dfiProBlockVnq;
+    } else if (this.poolLmCalculationTargetReturn === 'URTH'){
+      pool = this.poolUrth;
+      dfiPerBlock = this.dfiProBlockUrth;
+    } else if (this.poolLmCalculationTargetReturn === 'TLT'){
+      pool = this.poolTlt;
+      dfiPerBlock = this.dfiProBlockTlt;
+    } else if (this.poolLmCalculationTargetReturn === 'PDBC'){
+      pool = this.poolPdbc;
+      dfiPerBlock = this.dfiProBlockPdbc;
     } else {
       pool = this.poolUsdc;
       dfiPerBlock = this.dfiProBlockUsdc;
