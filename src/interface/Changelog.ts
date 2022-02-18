bcd9e58d0ad6c50444365f5148abee1e6d89990a
igorshelkovenkov <is@topiet.de>
2022-02-18 12:16:45 +0100

    bump

--- src/interface/Changelog.ts
+++ src/interface/Changelog.ts
@@ -12,6 +12,11 @@ export class Changelog {
     this.logs = new Array<Log>();
 
     this.log = new Log();
+    this.log.version = '8.12.24 - 18.2.2022';
+    this.log.comment = 'Add new language, welcome NL';
+    this.logs.push(this.log);
+
+    this.log = new Log();
     this.log.version = '8.12.23 - 18.2.2022';
     this.log.comment = 'Auto Update (5 s) User after add, remove or import addresses, fix history title';
     this.logs.push(this.log);
