1ce275bb01150d68c60b8daf1cc7ee9eac355060
Antoni Kucharczyk <hello@antoni.work>
2022-03-13 12:00:26 +0100

    Fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5454,10 +5454,10 @@ export class AppComponent implements OnInit {
 
   handlePageHeight(): void {
     document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
+    window.dispatchEvent(new Event('resize'));
 
     window.addEventListener('resize', () => {
-      const vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
+      document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
     });
   }
 
