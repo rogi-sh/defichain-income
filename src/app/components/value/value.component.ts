620f62443ba779cf7826f29f40a57e064624db32
igorshelkovenkov <is@topiet.de>
2022-03-24 15:35:07 +0100

    vaults sort by colleteral

--- src/app/components/value/value.component.ts
+++ src/app/components/value/value.component.ts
@@ -472,7 +472,7 @@ export class ValueComponent implements OnInit, OnChanges {
       vaults.push(...va.data);
     });
 
-    vaults.sort((a, b) => (a.state > b.state) ? 1 : ((b.state > a.state) ? -1 : 0));
+    vaults.sort((a, b) => (+a.collateralValue > +b.collateralValue) ? 1 : ((+b.collateralValue > +a.collateralValue) ? -1 : 0));
 
     return vaults;
 
