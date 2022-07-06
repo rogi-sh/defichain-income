90b837b6b4b0a1376dabb2e4ee7f12e7575b7dba
igorshelkovenkov <is@topiet.de>
2022-07-06 15:53:48 +0200

    typo

--- src/interface/Changelog.ts
+++ src/interface/Changelog.ts
@@ -13,7 +13,7 @@ export class Changelog {
 
     this.log = new Log();
     this.log.version = '9.2.7 - 6.7.2022';
-    this.log.comment = 'Fix history >= 3m preiod oracle price';
+    this.log.comment = 'Fix history >= 3m period oracle price';
     this.logs.push(this.log);
 
     this.log = new Log();
